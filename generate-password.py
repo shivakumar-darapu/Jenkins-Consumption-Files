e1e9cd75c23dd8b0d68d41dd65c0103e9c7731b3
skdarapu <darapushivakumar@gmail.com>
2024-08-30 11:55:46 +0530

    Create generate-password.py

---
+++ generate-password.py
@@ -0,0 +1,13 @@
+import os
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+if __name__ == "__main__":
+    new_password = generate_password()
+    os.environ['NEW_PASSWORD'] = new_password
+    print(f"Password generated and stored in environment variable.")
