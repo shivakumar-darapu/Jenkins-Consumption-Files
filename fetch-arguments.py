b80753c38d9d62996304a620886c81fc6c9f2509
skdarapu <darapushivakumar@gmail.com>
2024-09-03 16:29:21 +0530

    Update fetch-arguments.py

--- fetch-arguments.py
+++ fetch-arguments.py
@@ -4,8 +4,10 @@ def main():
     if len(sys.argv) > 1:
         argument = sys.argv[1]
         print(f"Argument received: {argument}")
+        sys.exit(0)
     else:
         print("No argument provided.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
